3409c99dcaf122fa84c5f0ccba2d871c6291f274
Tchuv <tchuvashov1706@gmail.com>
2023-03-22 23:01:15 +1200

    Json не идёт

--- src/channel.py
+++ src/channel.py
@@ -28,10 +28,17 @@ class Channel:
         return service
 
     def _init_from_api(self) -> None:
-        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
         self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = int(channel['items'][0]['statistics']['videoCount'])
         self.view_count = int(channel['items'][0]['statistics']['viewCount'])
+
+    def to_json(self):
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        with open(vdud.json,"w") as write_file:
+            json.dumps(channel, write_file)
+            return vdud.json
+
